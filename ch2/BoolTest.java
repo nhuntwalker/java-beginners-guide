a352755572f4d0632f7b55605315d003b6f32cd4
nhuntwalker <nhuntwalker@Gmail.com>
2018-11-25 15:37:52 -0800

    Worked through chapter 2. Onward to 3!

---
+++ ch2/BoolTest.java
@@ -0,0 +1,12 @@
+/**
+ * BoolTest
+ */
+public class BoolTest {
+
+    public static void main(String args[]){
+        boolean i;
+
+        i = 2;
+        System.out.println(i);
+    }
+}
---
+++ ch2/Sound.java
@@ -0,0 +1,12 @@
+/**
+ * Sound
+ */
+public class Sound {
+
+    public static void main(String args[]) {
+        double dist;
+        dist = 7.2 * 1100;
+
+        System.out.println("This lightning is " + dist + " feet away");
+    }    
+}
